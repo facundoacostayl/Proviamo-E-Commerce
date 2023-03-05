05db5a441b99dc63e13b600cf744de831361e40f
facundoacostayl <facundoacostayl@outlook.com>
2023-03-05 17:59:12 -0300

    Add env value to mercadopago config file

--- server/src/config/payway/mercadopago.js
+++ server/src/config/payway/mercadopago.js
@@ -1,10 +1,10 @@
 //  Mercado Pago's SDK
 const mercadopago = require("mercadopago");
-require('dotenv').config();
+require("dotenv").config();
 
 // Credentials
 mercadopago.configure({
-  access_token: "APP_USR-8484108289794507-112514-8e5d0769cbe0ddd38747a8c83d5e63e2-1190318373",
+  access_token: process.env.ACCESS_TOKEN_MP,
 });
 
 module.exports = mercadopago;
