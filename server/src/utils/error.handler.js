90bce2f9ba05eaec44e95bc24994b08b4c98e350
facundoacostayl <facundoacostayl@outlook.com>
2023-03-05 18:20:39 -0300

    Add response.handler file in utils folder

---
+++ server/src/utils/error.handler.js
@@ -0,0 +1,22 @@
+//Class created in order to extend the class "Error" and add new properties as statusCode as in this case
+class ErrorWithStatus extends Error {
+  status = 0;
+
+  get statusCode() {
+    return this.status;
+  }
+
+  set statusCode(code) {
+    this.status = code;
+  }
+}
+
+//Function for throwing a new ErrorWithStatus
+const throwErrorWithStatus = (response) => {
+  const error = new ErrorWithStatus(response.message);
+  error.statusCode = response.statusCode;
+
+  throw error;
+};
+
+module.exports = { ErrorWithStatus, throwErrorWithStatus };
