5c4edbbeb112458d2a20ff819ec04d55fbc85fcb
facundoacostayl <facundoacostayl@outlook.com>
2023-03-05 17:57:39 -0300

    Add new env values

---
+++ server/src/config/db/oAuth2client.js
@@ -0,0 +1,16 @@
+const { google } = require("googleapis");
+require("dotenv").config();
+
+const oAuth2Client = new google.auth.OAuth2(process.env.ID_OAUTH2CLIENT);
+
+oAuth2Client.setCredentials({
+  access_token: process.env.ACCESS_TOKEN_OAUTH2,
+  refresh_token: process.env.REFRESH_TOKEN_OAUTH2,
+  scope: "https://www.googleapis.com/auth/spreadsheets",
+  token_type: "Bearer",
+  expiry_date: 1641929044877,
+});
+
+const sheets = google.sheets({ version: "v4", auth: oAuth2Client });
+
+module.exports = { oAuth2Client, sheets };
--- server/src/services/products.js
+++ server/src/services/products.js
@@ -1,40 +1,51 @@
-const { google } = require('googleapis');
-const {sheets} = require('../config/oAuth2client');
+const { google } = require("googleapis");
+const { sheets } = require("../config/oAuth2client");
+require("dotenv").config();
 
-const getProducts = async() => {
-    const response = await sheets.spreadsheets.values.get({
-        spreadsheetId: '1NHHU7-mhlgVMaTRtyFi2zCTLk5mZzSelGX_xImdq_bI',
-        range: 'Productos!A2:I',
-    });
-    const rows = response.data.values;
-    const products = rows.map((row) => ({
-        id: +row[0],
-        title: row[1],
-        price: +row[2],
-        image: row[3],
-        category: row[4],
-        subcategory: row[5],
-        description: row[6],
-        fiambres: row[7],
-        quesos: row[8]
-    }));
+const getProducts = async () => {
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.ID_GSPREADSHEETS,
+    range: "Productos!A2:I",
+  });
+  const rows = response.data.values;
+  const products = rows.map((row) => ({
+    id: +row[0],
+    title: row[1],
+    price: +row[2],
+    image: row[3],
+    category: row[4],
+    subcategory: row[5],
+    description: row[6],
+    fiambres: row[7],
+    quesos: row[8],
+  }));
 
-    return products;
-}
+  return products;
+};
 
-const addProducts = async(products) => {
-    let values = products.map(p => [p.id, p.title, p.price, p.image, p.category, p.subcategory, p.description, p.fiambres, p.quesos]);
+const addProducts = async (products) => {
+  let values = products.map((p) => [
+    p.id,
+    p.title,
+    p.price,
+    p.image,
+    p.category,
+    p.subcategory,
+    p.description,
+    p.fiambres,
+    p.quesos,
+  ]);
 
-    const resource = {
-        values,
-    };
+  const resource = {
+    values,
+  };
 
-    const result = await sheets.spreadsheets.values.update({
-        spreadsheetId: '1NHHU7-mhlgVMaTRtyFi2zCTLk5mZzSelGX_xImdq_bI',
-        range: 'Productos!A2:I',
-        valueInputOption: "RAW",
-        resource,
-    });
-}
+  const result = await sheets.spreadsheets.values.update({
+    spreadsheetId: process.env.ID_GSPREADSHEETS,
+    range: "Productos!A2:I",
+    valueInputOption: "RAW",
+    resource,
+  });
+};
 
-module.exports = {getProducts, addProducts};
+module.exports = { getProducts, addProducts };
