364e286e4e75cb96f34d4a963f86ee5edd92c6f3
facundoacostayl <facundoacostayl@outlook.com>
2023-03-05 18:12:12 -0300

    Update addItem in product controller

--- server/src/controllers/products.js
+++ server/src/controllers/products.js
@@ -1,21 +1,21 @@
-const {errorHandler} = require('../utils/error.handle');
-const {getProducts, addProducts} = require('../services/products');
+const { errorHandler } = require("../utils/error.handle");
+const { getProducts, addProducts } = require("../services/products");
 
-const getItems = async(req, res) => {
-    try {
-        const response = await getProducts();
-        res.send(response);
-    }catch(e) {
-        errorHandler(res, "ERROR_GET_PRODUCTS", 400);
-    }
+const getItems = async (req, res) => {
+  try {
+    const response = await getProducts();
+    res.send(response);
+  } catch (e) {
+    errorHandler(res, "ERROR_GET_PRODUCTS", 400);
+  }
 };
 
-const addItems = async(req, res) => {
-    try {
-        res.send('IT WORKS AGAIN BROOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!');
-    }catch(e) {
-        errorHandler(res, "ERROR_ADD_PRODUCTS", 400);
-    }
+const addItems = async (req, res) => {
+  try {
+    res.sendStatus(200);
+  } catch (e) {
+    errorHandler(res, "ERROR_ADD_PRODUCTS", 400);
+  }
 };
 
-module.exports = {getItems, addItems};
+module.exports = { getItems, addItems };
