56f9abbbdbe3bce2ff4b5635dbe968518fdfea5c
unknown <facundoacostayl@outlook.com>
2023-03-08 20:58:51 +0100

    Update addItems function controller

--- server/src/controllers/orders.js
+++ server/src/controllers/orders.js
@@ -1,42 +1,50 @@
-const {getOrders, addOrders, getOrderId, updateOrders, getOrderStatus} = require('../services/orders');
-const {errorHandler} = require('../utils/error.handle');
+const {
+  getOrders,
+  addOrders,
+  getOrderProducts,
+  getOrderId,
+  updateOrders,
+  getOrderStatus,
+} = require("../services/orders");
+const { errorHandler } = require("../utils/error.handle");
 
-const getItems = async(req, res) => {
-    const response = await getOrders();
+const getItems = async (req, res) => {
+  const response = await getOrders();
 
-    res.send(response);
-}
+  res.send(response);
+};
 
-const addItems = async({body}, res) => {
-    try {
-        const response = await getOrderId(body);
-        await addOrders(body, response);
-        res.send({response});
-    }catch(e) {
-        errorHandler(res, e.message, 400);
-    }
-}
+const addItems = async ({ body }, res) => {
+  try {
+    const responseOrder = await getOrderProducts(body);
+    const responseId = await getOrderId(responseOrder);
+    await addOrders(body, response);
+    res.send({ response });
+  } catch (e) {
+    errorHandler(res, e.message, 400);
+  }
+};
 
-const updateItems = async(req, res) => {
-    try {
-        const response = await getOrderStatus();
-        await updateOrders(response.preferenceId, response.status);
-        res.sendFile(require.resolve("./frontend/index.html"));
-    }catch(e) {
-        errorHandler(res, e.message, 400);
-    }
-}
+const updateItems = async (req, res) => {
+  try {
+    const response = await getOrderStatus();
+    await updateOrders(response.preferenceId, response.status);
+    res.sendFile(require.resolve("./frontend/index.html"));
+  } catch (e) {
+    errorHandler(res, e.message, 400);
+  }
+};
 
-const updateItemById = async(req, res) => {
-    try {
-        const response = await getOrderStatus();
-        const orders = await getItems();
-        const order = orders.find(o => o.preferenceId === response.preferenceId);
-        order.status = response.status;
-        await writeOrders(orders);
-    }catch(e) {
-        errorHandler(res, e.message, 400);
-    }
-}
+const updateItemById = async (req, res) => {
+  try {
+    const response = await getOrderStatus();
+    const orders = await getItems();
+    const order = orders.find((o) => o.preferenceId === response.preferenceId);
+    order.status = response.status;
+    await writeOrders(orders);
+  } catch (e) {
+    errorHandler(res, e.message, 400);
+  }
+};
 
-module.exports = {getItems, addItems, updateItems};
+module.exports = { getItems, addItems, updateItems };
