f5f3b0428ca4438790995a37d1fe5f437cfd6cdf
unknown <facundoacostayl@outlook.com>
2023-03-16 16:50:59 +0100

    Add comments in orders service

--- server/src/services/orders.js
+++ server/src/services/orders.js
@@ -5,11 +5,13 @@ const { responseHandler } = require("../utils/response.handler");
 const { httpStatusCodes } = require("../utils/httpStatusCodes");
 
 const getOrders = async () => {
+  //GET Orders from DB
   const response = await sheets.spreadsheets.values.get({
     spreadsheetId: "1csLKz4P6rmXNs633SgqAF3Gn6ktb8B6Y4zbOD7sYA84",
     range: "Orders!A2:E",
   });
 
+  //CHECK if orders exists, otherwise return error
   if (!response) {
     return responseHandler(
       "Error",
@@ -18,7 +20,10 @@ const getOrders = async () => {
     );
   }
 
+  //GET orders data from response
   const rows = response.data.values || [];
+
+  //CREATE a new array of orders getted from rows
   const orders = rows.map((row) => ({
     preferenceId: row[0],
     items: JSON.parse(row[1]),
@@ -36,8 +41,10 @@ const getOrders = async () => {
 };
 
 const addOrders = async (order, preferenceId) => {
+  //GET Orders from getOrders function
   const orders = await getOrders();
 
+  //CHECK if orders exists, otherwise return error
   if (!orders.data) {
     return responseHandler(
       "Error",
@@ -46,14 +53,20 @@ const addOrders = async (order, preferenceId) => {
     );
   }
 
+  //GET orders data from orders
   const ordersData = orders.data;
 
+  //INSERT "date" key in order called from arguments
   order.date = new Date().toISOString();
+  //INSERT "preferenceId" key in order called from arguments
   order.preferenceId = preferenceId;
+  //INSERT "status" key in order called from arguments
   order.status = "Pendiente";
 
+  //PUSH items from orders into ordersData
   ordersData.push(order.items);
 
+  //Create a new array with updated ordersData values
   let values = ordersData.map((order) => [
     order.preferenceId,
     JSON.stringify(order.items),
@@ -62,18 +75,21 @@ const addOrders = async (order, preferenceId) => {
     order.status,
   ]);
 
+  //INSERT values array in an object
   const resource = {
     values,
   };
 
-  const response = await sheets.spreadsheets.values.update({
+  //UPDATE DB inserting resource object
+  const result = await sheets.spreadsheets.values.update({
     spreadsheetId: "1csLKz4P6rmXNs633SgqAF3Gn6ktb8B6Y4zbOD7sYA84",
     range: "Orders!A2:E",
     valueInputOption: "RAW",
     resource,
   });
 
-  if (!response) {
+  //CHECK if result went ok, otherwise return error
+  if (!result) {
     return responseHandler(
       "Error",
       httpStatusCodes.BAD_REQUEST,
@@ -90,6 +106,7 @@ const addOrders = async (order, preferenceId) => {
 };
 
 const updateOrders = async (orders) => {
+  //CREATE a new array of orders getted from function argument
   const values = orders.map((order) => [
     order.preferenceId,
     JSON.stringify(order.finalOrder),
@@ -99,18 +116,21 @@ const updateOrders = async (orders) => {
     order.status,
   ]);
 
+  //INSERT values array in an object
   const resource = {
     values,
   };
 
-  const response = await sheets.spreadsheets.values.update({
+  //UPDATE DB inserting resource object
+  const result = await sheets.spreadsheets.values.update({
     spreadsheetId: "1csLKz4P6rmXNs633SgqAF3Gn6ktb8B6Y4zbOD7sYA84",
     range: "Orders!A2:F",
     valueInputOption: "RAW",
     resource,
   });
 
-  if (!response) {
+  //CHECK if result went ok, otherwise return error
+  if (!result) {
     return responseHandler(
       "Error",
       httpStatusCodes.BAD_REQUEST,
@@ -118,12 +138,14 @@ const updateOrders = async (orders) => {
     );
   }
 
-  return responseHandler("Success", 200, "Order updated succesfully", response);
+  return responseHandler("Success", 200, "Order updated succesfully", values);
 };
 
 const getOrderPreference = async (order) => {
+  //GET Products from getProducts products service function
   const productsCopy = await getProducts();
 
+  //CHECK if productsCopy exists, otherwise return error
   if (!productsCopy.data) {
     return responseHandler(
       "Error",
@@ -132,8 +154,10 @@ const getOrderPreference = async (order) => {
     );
   }
 
+  //GET id values of items from order function argument
   const ids = order.items.map((p) => p.id);
 
+  //CREATE preference value for Mercado Pago
   let preference = {
     items: [],
     back_urls: {
@@ -144,6 +168,7 @@ const getOrderPreference = async (order) => {
     auto_return: "approved",
   };
 
+  //GET products from productsCopy based in order ids and pushing them into preference object items value
   ids.forEach((id) => {
     const product = productsCopy.data.find((p) => p.id === id);
     preference.items.push({
@@ -162,8 +187,10 @@ const getOrderPreference = async (order) => {
 };
 
 const getOrderPreferenceId = async (preference) => {
+  //CREATE Mercado Pago preference id and getting request response
   const response = await mercadopago.preferences.create(preference);
 
+  //CHECK if response exists, otherwise return
   if (!response) {
     return responseHandler(
       "Error",
@@ -172,22 +199,22 @@ const getOrderPreferenceId = async (preference) => {
     );
   }
 
+  //CREATE a new variable with the preference id getted from response
   const preferenceId = response.body.id;
+
   return responseHandler(
     "Success",
     httpStatusCodes.OK,
     "Preference id found succesfully",
     preferenceId
   );
-  /*order.date = new Date().toISOString();
-  order.preferenceId = preferenceId;
-  order.status = "Pendiente";
-  const orders = await getOrders();*/
 };
 
 const updateOrderStatus = async (preferenceId, status) => {
+  //GET Orders from getOrders function
   const orders = await getOrders();
 
+  //CHECK if orders data exists, otherwise return
   if (!orders.data) {
     return responseHandler(
       "Error",
@@ -196,19 +223,26 @@ const updateOrderStatus = async (preferenceId, status) => {
     );
   }
 
+  //CREATE a new variable with orders data
   const ordersData = orders.data;
 
+  //GET order to be updated data
   const orderToUpdate = ordersData.find((o) => o.preferenceId === preferenceId);
+  //GET order to be updated index
   const orderToUpdateIndex = ordersData.findIndex(
     (o) => o.preferenceId === preferenceId
   );
 
+  //UPDATE order to be updated status value with status argument
   orderToUpdate.status = status;
+  //UPDATE ordersData to be orderToUpdate
   ordersData[orderToUpdateIndex] = orderToUpdate;
-
+  //PUSH orderToUpdate into ordersData
   const newOrders = ordersData.push(orderToUpdate);
+  //UPDATE orders from DB with updateOrders function
   const orderUpdated = await updateOrders(newOrders);
 
+  //CHECK if orderUpdated went ok, otherwise return
   if (!orderUpdated.data) {
     return responseHandler(
       "Error",
@@ -226,8 +260,10 @@ const updateOrderStatus = async (preferenceId, status) => {
 };
 
 const getOrderStatus = async () => {
+  //GET payment id from Mercado Pago
   const payment = await mercadopago.payment.findById(req.query.payment_id);
 
+  //CHECK if payment exists, otherwise return
   if (!payment) {
     return responseHandler(
       "Error",
@@ -236,10 +272,12 @@ const getOrderStatus = async () => {
     );
   }
 
+  //GET merchant order from Mercado Pago
   const merchantOrder = await mercadopago.merchant_orders.findById(
     payment.body.order.id
   );
 
+  //CHECK if merchantOrder exists, otherwise return
   if (!merchantOrder) {
     return responseHandler(
       "Error",
@@ -248,7 +286,9 @@ const getOrderStatus = async () => {
     );
   }
 
+  //CREATE a new variable with merchantOrder data
   const preferenceId = merchantOrder.body.preference_id;
+  //CREATE a new variable with payment data
   const status = payment.body.status;
 
   return responseHandler("Success", 200, "Order Status found succesfully", {
